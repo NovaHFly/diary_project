9faea361a8833b033749f405327703c513707a54
NovaHFly <mugiviska@gmail.com>
2025-04-04 09:52:41 +0300

    feat: Add custom user model

--- diary_project/diary_project/settings.py
+++ diary_project/diary_project/settings.py
@@ -28,6 +28,8 @@ ALLOWED_HOSTS = os.getenv(
     'localhost,127.0.0.1',
 ).split(',')
 
+AUTH_USER_MODEL = 'users.User'
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -36,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'diary',
+    'api',
 ]
 
 MIDDLEWARE = [
