dc62b0ff86eb04e7a5f999542244e61a2fc46991
NovaHFly <mugiviska@gmail.com>
2025-04-07 00:14:31 +0300

    feat: Add models to admin

--- diary_project/apps/diary/admin.py
+++ diary_project/apps/diary/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Note)
+class Note(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    list_display_links = ['title']
+    search_fields = ['title']
+
+
+@admin.register(models.Tag)
+class Tag(admin.ModelAdmin):
+    list_display = ['name', 'author']
+    list_display_links = ['name']
+    search_fields = ['name']
