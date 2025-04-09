bd54f4cb78ecfd9ec990457715b38f11d069bdd1
NovaHFly <mugiviska@gmail.com>
2025-04-10 00:05:22 +0300

    fix: Replace file_path with text field

--- diary_project/apps/api/serializers.py
+++ diary_project/apps/api/serializers.py
@@ -1,9 +1,6 @@
-from pathlib import Path
-
 import rest_framework.serializers as serializers
 
 from diary.models import Note, Tag
-from diary.util import write_to_random_file
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -32,7 +29,6 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class NoteSerializer(serializers.ModelSerializer):
-    text = serializers.CharField()
     tags = TagSerializer(many=True)
 
     class Meta:
@@ -47,12 +43,8 @@ class NoteSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         tags = validated_data.pop('tags')
-        text = validated_data.pop('text')
 
-        file_path = str(write_to_random_file(text))
         validated_data['author'] = self.context['request'].user
-        validated_data['file_path'] = file_path
-
         note = super().create(validated_data)
 
         for tag in tags:
@@ -63,11 +55,8 @@ class NoteSerializer(serializers.ModelSerializer):
 
     def update(self, instance: Note, validated_data):
         tags = validated_data.pop('tags')
-        text = validated_data.pop('text')
 
         super().update(instance, validated_data)
-
-        Path(instance.file_path).write_text(text)
 
         tags_list = [Tag.objects.get_or_create(**tag) for tag in tags]
         instance.tags.set(tags_list)
--- diary_project/apps/diary/models.py
+++ diary_project/apps/diary/models.py
@@ -1,10 +1,5 @@
-from pathlib import Path
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
-
-DATA_PATH: Path = settings.DATA_PATH
 
 User = get_user_model()
 
@@ -31,13 +26,9 @@ class Note(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=100)
-    file_path = models.FilePathField(path=str(DATA_PATH))
+    text = models.TextField()
 
     tags = models.ManyToManyField(Tag)
-
-    @property
-    def text(self) -> str:
-        return Path(self.file_path).read_text()
 
     def __str__(self) -> str:
         return self.title
